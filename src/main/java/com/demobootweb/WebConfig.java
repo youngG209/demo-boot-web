964e5c7dccaf143335e3e9232315ea29445ad803
youngG209 <young209@mcocoa.com>
2021-02-23 17:45:43 +0900

    핸들러 인터셉터 - preHandle : 핸들러 실행 전 호출 - postHandle : 핸들러 실행 후, 아직 뷰 랜더링 하기 전에 호출 - afterCompletion : 요청처리가 완전히 끝난 뒤 (뷰 랜더링 까지 끝난 뒤) 호출 - config 등록 -> addInterceptors : 등록하여 해당 핸들러 사용  * .addInterceptor(new 핸들러 명)  * .order(숫자) : 우선순위  * .addPathPatterns(특정패턴) : 해당 패턴에만 적용

--- src/main/java/com/demobootweb/WebConfig.java
+++ src/main/java/com/demobootweb/WebConfig.java
@@ -2,13 +2,24 @@ package com.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
 //    @Override
 //    public void addFormatters(FormatterRegistry registry) {
 //        registry.addFormatter(new PersonFormatter());
 //    }
-//}
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        .order(숫자) : 우선순위
+//        .addPathPatterns(특정패턴) : 해당 패턴에만 적용
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi")
+                .order(-1);
+    }
+}
